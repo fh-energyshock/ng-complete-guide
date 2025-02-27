b0b3a2511eba5f13291c6400325072705b409f8c
Manuel Brestak <manuel.brestak@gmail.com>
2025-02-27 09:45:36 +0100

    computed signals

--- angular-course/src/app/ng-dev/signal-tutorial/signal-tutorial.component.ts
+++ angular-course/src/app/ng-dev/signal-tutorial/signal-tutorial.component.ts
@@ -15,37 +15,21 @@ import {
   styleUrl: './signal-tutorial.component.scss',
 })
 export class SignalTutorialComponent implements OnInit {
-  firstName = signal('Manuel');
-  firstNameLowerCase: Signal<string> = signal('');
-  showDate = signal(false);
-  isDateExpired = signal(false);
-  showDateDuration = computed(() => this.showDate() && !this.isDateExpired());
-  timestamp: WritableSignal<Date | null> = signal(null);
-  showCurrentTimestamp = computed(() => this.timestamp());
+  // Only the signals actually read during the derivation are tracked. For example, in this computed the count signal is only read if the showCount signal is true
+  showCount = signal(false);
   count = signal(0);
+  conditionalCount = computed(() => {
+    if (this.showCount()) {
+      return `The count is ${this.count()}.`;
+    } else {
+      return 'Nothing to see here!';
+    }
+  });
 
-  ngOnInit(): void {
-    // A computed is a signal that produces its value based on other signals.
-    // A computed signal is read-only; it does not have a set or an update method.
-    this.firstNameLowerCase = computed(() => this.firstName().toLowerCase());
-    console.log(this.firstNameLowerCase());
-    this.firstName.set('Manu');
-    console.log(this.firstNameLowerCase());
+  ngOnInit(): void {}
 
-    // Writable signals provide an API for updating their values directly.
-    console.log('The count is: ', this.count());
-    this.count.update((value) => {
-      return (value += 1);
-    });
-    console.log('The count is: ', this.count());
-  }
-
-  activateDate(): void {
-    this.timestamp.set(new Date());
-    this.showDate.set(true);
-  }
-
-  deactivateDate(): void {
-    this.showDate.set(false);
+  updateCount(): void {
+    this.showCount.set(true);
+    this.count.update((value) => value + 1);
   }
 }
